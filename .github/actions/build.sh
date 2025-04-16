0fb3e5dd383eaa3c91ca14e5183af50faf74e562
fangcongyang <765463281@qq.com>
2025-04-17 00:37:57 +0800

    ci: 更新Linux构建脚本和依赖项
    - 在`build.sh`中添加`rpm`包构建支持
    - 修正`action.yml`中的Docker镜像路径
    - 使用`jq`和`python3`替换`sed`命令以更新版本号
    - 更新`entrypoint.sh`中的依赖项，使用`libayatana-appindicator3-dev`替换`libappindicator3-dev`

--- .github/actions/build.sh
+++ .github/actions/build.sh
@@ -3,5 +3,5 @@ pnpm install
 if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
     pnpm tauri build --target $INPUT_TARGET
 else
-    pnpm tauri build --target $INPUT_TARGET -b deb
+    pnpm tauri build --target $INPUT_TARGET -b deb rpm
 fi
