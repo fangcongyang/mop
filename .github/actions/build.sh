d59029500b2b74893c4f2399f818c30694193dc9
fangcongyang <765463281@qq.com>
2025-04-18 01:05:03 +0800

    ci: 移除Linux构建相关文件并更新release.yml
    移除了不再使用的Linux构建相关文件，包括Dockerfile、build.sh、action.yml和entrypoint.sh。同时，更新了release.yml文件，增加了对macOS平台的OpenSSL依赖安装步骤，以支持不同架构的构建。

--- .github/actions/build.sh
+++
@@ -1,7 +0,0 @@
-# pnpm install --resolution-only
-pnpm install
-if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
-    pnpm tauri build --target $INPUT_TARGET
-else
-    pnpm tauri build --target $INPUT_TARGET -b deb rpm
-fi
