d476f8f8eb52b35af390dc9457644e1ad31348f7
fangcongyang <765463281@qq.com>
2025-04-18 09:54:00 +0800

    feat: 添加自动更新功能并配置相关依赖
    此次提交引入了自动更新功能，包括添加了 `tauri-plugin-updater` 插件，并在 `tauri.conf.json` 中配置了更新端点。同时，更新了 `Cargo.toml` 和 `package.json` 以支持自动更新功能，并添加了相关的 CI 配置和 changelog 生成工具。

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -30,6 +30,7 @@ pub static APP: OnceCell<tauri::AppHandle> = OnceCell::new();
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
